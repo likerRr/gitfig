8cdd31c2d8baf7703445e8dd8066b09bafc3c21d
Alexey Lizurchik <al.lizurchik@gmail.com>
2017-03-20 12:09:10 +0300

    Fix test

--- test.js
+++ test.js
@@ -5,5 +5,5 @@ test('api is consistence', t => {
 	t.is(typeof gitFig, 'function');
 	t.is(typeof gitFig.sync, 'function');
 	t.is(typeof gitFig.LOCAL, 'number');
-	t.is(typeof gitFig.GLOBAL, 'number');
+	t.is(typeof gitFig.HOME, 'number');
 });
